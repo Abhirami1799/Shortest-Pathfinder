464df6af17ea3304b1e6889574106146066ede84
Gowri Jayaprakash <34152914+gowrijp@users.noreply.github.com>
2020-07-11 11:44:59 +0530

    Update utilities.js

--- src/scripts/utilities.js
+++ src/scripts/utilities.js
@@ -1,11 +1,14 @@
-// utilities
 function uuidv4() {
+  var dt= new Date().getTime();
   return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, function(c) {
-    var r = (Math.random() * 16) | 0,
+    var r = (dt + Math.random() * 16)%16 | 0;
+    dt = Math.floor(dt/16);
       v = c == "x" ? r : (r & 0x3) | 0x8;
     return v.toString(16);
   });
 }
+
+console.log(uuidv4());
 
 function openFullscreen() {
   if (window.innerHeight == screen.height) {
@@ -36,134 +39,4 @@ function openFullscreen() {
       elem.msRequestFullscreen();
     }
   }
-}
-
-class LNode {
-  constructor(value) {
-    this.__next = null;
-    this.__value = value;
-    this.__prev = null;
-  }
-
-  set next(node) {
-    this.__next = node;
-  }
-
-  set prev(node) {
-    this.__prev = node;
-  }
-
-  set value(value) {
-    this.__value = value;
-  }
-
-  get value() {
-    return this.__value;
-  }
-
-  get next() {
-    return this.__next;
-  }
-
-  get prev() {
-    return this.__prev;
-  }
-}
-class Queue {
-  constructor() {
-    this.__head = null;
-    this.__tail = null;
-    this.__size = 0;
-  }
-
-  enqueue(...values) {
-    for (let i = 0; i < values.length; i++) {
-      const node = new LNode(values[i]);
-
-      if (!this.__head) {
-        this.__head = node;
-        this.__tail = this.__head;
-      } else {
-        this.__tail.next = node;
-        this.__tail = this.__tail.next;
-      }
-
-      this.__size++;
-    }
-  }
-
-  dequeue() {
-    if (this.__head) {
-      const temp_node = this.head.value;
-      this.__head = this.__head.next;
-      this.__size--;
-      return temp_node;
-    } else return null;
-  }
-
-  get size() {
-    return this.__size;
-  }
-
-  get tail() {
-    return this.__tail;
-  }
-
-  get head() {
-    return this.__head;
-  }
-}
-
-class Stack {
-  constructor() {
-    this.__head = null;
-    this.__tail = null;
-    this.__size = 0;
-  }
-
-  push(...values) {
-    for (let i = 0; i < values.length; i++) {
-      const node = new LNode(values[i]);
-
-      if (!this.__head) {
-        this.__head = node;
-        this.__tail = this.__head;
-      } else {
-        node.prev = this.__tail;
-        this.__tail.next = node;
-        this.__tail = this.__tail.next;
-      }
-
-      this.__size++;
-    }
-  }
-
-  pop() {
-    if (this.__tail) {
-      const temp_node = this.__tail.value;
-
-      if (this.__head == this.__tail) {
-        this.__head = null;
-      }
-      this.__tail = this.__tail.prev;
-      this.__size--;
-      return temp_node;
-    } else return null;
-  }
-
-  peek() {
-    if (this.__tail) {
-      return this.__tail.value;
-    }
-  }
-
-  get size() {
-    return this.__size;
-  }
-}
-
-function getRowColumnCount(boxSize) {
-  const row = Math.trunc(states.height / boxSize);
-  const column = Math.trunc(states.width / boxSize);
-  return [row, column];
 }
